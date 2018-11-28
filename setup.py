8a298a9228ed86b13cfeb92ccbc59d14debe14f9
parrt <parrt@cs.usfca.edu>
2018-11-28 11:02:19 -0800

    forgot colour lib dependence; bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtreeviz',
-    version='0.3',
+    version='0.3.1',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
-    install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn','matplotlib'],
+    install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn','matplotlib','colour'],
     python_requires='>=3.6',
     author='Terence Parr and Prince Grover',
     author_email='parrt@cs.usfca.edu',
