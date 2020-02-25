16649ed9ae966e28d329a948e69c86d2a4dd6350
tlapusan <tudor.lapusan@gmail.com>
2020-02-25 17:33:07 +0200

    Add docs and sklearn default feature importance for prediction path

--- dtreeviz/model_interpretation/prediction_path_interpretation.py
+++ dtreeviz/model_interpretation/prediction_path_interpretation.py
@@ -2,13 +2,15 @@
 Prediction path interpretation for decision tree models.
 In this moment, it contains "plain english" implementation, but others can be added in the future.
 """
-import numpy
+import numpy as np
 import pandas
 from sklearn import tree
+import matplotlib.pyplot as plt
+from dtreeviz.colors import adjust_colors
 
 
 def explain_prediction_plain_english(tree_model: (tree.DecisionTreeClassifier, tree.DecisionTreeRegressor),
-                                     X: (pandas.core.series.Series, numpy.ndarray),
+                                     X: (pandas.core.series.Series, np.ndarray),
                                      feature_names):
     """
     Explains the prediction path using feature value's range.
@@ -52,13 +54,10 @@ def explain_prediction_plain_english(tree_model: (tree.DecisionTreeClassifier, t
         elif feature_max_range.get(feature_name, feature_value) < feature_split_value:
             feature_max_range[feature_name] = feature_split_value
 
-    # TODO
-    # X is based on dataframe, we need to search into it using feature_name. Find a more general approach.
     for feature_name in feature_names:
         feature_range = ""
         if feature_name in feature_min_range:
             feature_range = f"{feature_min_range[feature_name]} <= {feature_name}"
-
         if feature_name in feature_max_range:
             if feature_range == "":
                 feature_range = f"{feature_name} < {feature_max_range[feature_name]}"
@@ -69,8 +68,86 @@ def explain_prediction_plain_english(tree_model: (tree.DecisionTreeClassifier, t
             print(feature_range)
 
 
-def explain_prediction_weights(tree_model, X):
-    return "to be implemented"
+def explain_prediction_sklearn_default(tree_model, X, features,
+                                       figsize: tuple = (10, 5),
+                                       colors: dict = None,
+                                       fontsize: int = 14,
+                                       fontname: str = "Arial",
+                                       grid: bool = False):
+    """
+    Explain prediction calculating features importance using sklearn default algorithm : mean decrease in impurity
+    (or gini importance) mechanism.
+    This mechanism can be biased, especially for situations where features vary in their scale of measurement or
+    their number of categories.
+    For more details, you can read this article : https://explained.ai/rf-importance/index.html
+
+    :param tree_model: tree used to make prediction
+    :param X: Instance example to make prediction
+    :param features: list
+        Feature name list
+    :param figsize: tuple of int, optional
+        The plot size
+    :param colors: dict, optional
+        The set of colors used for plotting
+    :param fontsize: int, optional
+        Plot labels fontsize
+    :param fontname: str, optional
+        Plot labels font name
+    :return:
+        Prediction feature's importance plot
+    """
+
+    node_indicator = tree_model.decision_path([X])
+    decision_node_path = node_indicator.indices[node_indicator.indptr[0]:
+                                                node_indicator.indptr[1]]
+    feature_path_importance = _get_feature_path_importance_sklearn(tree_model, decision_node_path)
+    return _get_feature_path_importance_sklearn_plot(features, feature_path_importance, figsize, colors, fontsize,
+                                                     fontname,
+                                                     grid)
+
+
+def _get_feature_path_importance_sklearn_plot(features, feature_path_importance, figsize, colors, fontsize, fontname,
+                                              grid):
+    colors = adjust_colors(colors)
+    fig, ax = plt.subplots(figsize=figsize);
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_linewidth(.3)
+    ax.spines['bottom'].set_linewidth(.3)
+    ax.set_xticks(range(0, len(features)))
+    ax.set_xticklabels(features)
+
+    barcontainers = ax.bar(range(0, len(features)), feature_path_importance, color=colors["hist_bar"], lw=.3,
+                           align='center',
+                           width=1)
+    for rect in barcontainers.patches:
+        rect.set_linewidth(.5)
+        rect.set_edgecolor(colors['rect_edge'])
+    ax.set_xlabel("features", fontsize=fontsize, fontname=fontname, color=colors['axis_label'])
+    ax.set_ylabel("feature importance", fontsize=fontsize, fontname=fontname, color=colors['axis_label'])
+    ax.grid(b=grid)
+
+    return ax
+
+
+def _get_feature_path_importance_sklearn(tree_model, node_list):
+    gini_importance = np.zeros(tree_model.tree_.n_features)
+    for node in node_list:
+        if tree_model.tree_.children_left[node] != -1:
+            node_left = tree_model.tree_.children_left[node]
+            node_right = tree_model.tree_.children_right[node]
+
+            gini_importance[tree_model.tree_.feature[node]] += tree_model.tree_.weighted_n_node_samples[node] * \
+                                                               tree_model.tree_.impurity[node] \
+                                                               - tree_model.tree_.weighted_n_node_samples[node_left] * \
+                                                               tree_model.tree_.impurity[node_left] \
+                                                               - tree_model.tree_.weighted_n_node_samples[node_right] * \
+                                                               tree_model.tree_.impurity[node_right]
+    normalizer = np.sum(gini_importance)
+    if normalizer > 0.0:
+        gini_importance /= normalizer
+
+    return gini_importance
 
 
 def get_prediction_explainer(explanation_type: str):
@@ -82,7 +159,7 @@ def get_prediction_explainer(explanation_type: str):
 
     if explanation_type == "plain_english":
         return explain_prediction_plain_english
-    elif explanation_type == "weights":
-        return explain_prediction_weights
+    elif explanation_type == "sklearn_default":
+        return explain_prediction_sklearn_default
     else:
         raise ValueError(f"Explanation type {explanation_type} is not supported yet!")
