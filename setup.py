9dae088ad28d5e9d13b648a2cd6e5b7535beb819
Terence Parr <parrt@cs.usfca.edu>
2020-07-05 17:56:21 -0700

    Update setup.py
    Set version to 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtreeviz',
-    version='0.8.2',
+    version='1.0',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
