f7813e1d4e0beccbc88539638819e554c7688d43
parrt <parrt@cs.usfca.edu>
2020-07-16 10:34:41 -0700

    update setup

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     packages=find_packages(),
     install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn','matplotlib','colour', 'xgboost', 'pytest'],
     python_requires='>=3.6',
-    author='Terence Parr and Prince Grover',
+    author='Terence Parr, Tudor Lapusan, and Prince Grover',
     author_email='parrt@cs.usfca.edu',
-    description='A Python 3 library for sci-kit learn decision tree visualization',
+    description='A Python 3 library for sci-kit learn and XGBoost decision tree visualization',
     keywords='machine-learning data structures trees visualization',
     classifiers=['License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers']
