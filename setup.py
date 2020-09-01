25181a924742fd4d67042e517b19d25fdbe8443b
parrt <parrt@cs.usfca.edu>
2020-09-01 10:18:54 -0700

    set 1.1 version for pyspark

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtreeviz',
-    version='1.0',
+    version='1.1',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
-    install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn','matplotlib','colour', 'xgboost', 'pytest'],
+    install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn','matplotlib','colour', 'xgboost', 'pytest', 'pyspark'],
     python_requires='>=3.6',
     author='Terence Parr, Tudor Lapusan, and Prince Grover',
     author_email='parrt@cs.usfca.edu',
