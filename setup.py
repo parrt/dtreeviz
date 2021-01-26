fbcb423aa51ef5f901310a5bd98852363d47a1a5
parrt <parrt@cs.usfca.edu>
2021-01-26 15:58:14 -0800

    Fixes #117 and bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtreeviz',
-    version='1.1.3',
+    version='1.1.4',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
