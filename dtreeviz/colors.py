dcc223f18a94f9319e2497ef0586a7e13c8ecff3
Tudor Lapusan <tudor.lapusan@gmail.com>
2022-07-24 19:13:17 +0300

    #187 dtreeviz dont reset color values (#188)
    * Correct function call `sklearn.utils.compute_class_weight`
    
    Closes #166
    
    * Fix the issue with color overwritten values and move the logic for choosing the color inside the colors.py
    
    Co-authored-by: Thom Sentner <56683103+thomsentner@users.noreply.github.com>

--- dtreeviz/colors.py
+++ dtreeviz/colors.py
@@ -10,7 +10,7 @@ LIGHTORANGE = '#fee090'
 LIGHTBLUE = '#a6bddb'
 GREY = '#444443'
 WEDGE_COLOR = GREY
-CATEGORICAL_SPLIT_LEFT= '#FFC300'
+CATEGORICAL_SPLIT_LEFT = '#FFC300'
 CATEGORICAL_SPLIT_RIGHT = BLUE
 
 HIGHLIGHT_COLOR = '#D67C03'
@@ -29,16 +29,17 @@ color_blind_friendly_colors = [
     ['#FEFEBB', '#c7e9b4', '#41b6c4', '#74add1', BLUE, DARKBLUE, LIGHTORANGE, '#fdae61', '#f46d43', '#d73027']  # 10
 
 ]
-def get_colorblind_friendly_colors():
-    colors  = color_blind_friendly_colors
 
-    return colors
 
-def get_hex_colors(n_classes,cmap_name = "RdYlBu"):
-    """will generate a list of lists that contain n discrete hex colors
-     from a given matplotlib colormap based on the number of classes in 
-     a given classifier model as determined in trees.py. Defaults to the 
-     "RdYlBu" colormap. 
+def get_hex_colors(n_classes, cmap_name="RdYlBu"):
+    """
+    Will generate a list of lists that contain n discrete hex colors
+    from a given matplotlib colormap based on the number of classes in
+    a given classifier model as determined in trees.py. Defaults to the
+    "RdYlBu" colormap.
+
+    For backward compatibility with the color_blind_friendly_colors, the first 10 lists will be populated with values
+    from color_blind_friendly_colors list.
 
     Args:
         n_classes (int): the number of classes in a classifier model as determined 
@@ -47,49 +48,29 @@ def get_hex_colors(n_classes,cmap_name = "RdYlBu"):
 
     Returns:
         list: a list of lists where each inner list item contains n discrete hex colors.
-        Ex:
-        get_hex_colors(10)
-
-        [
-            None, # 0 classes
-            None, # 1 class
-            ['#a50026ff', '#313695ff'], # 2 classes
-            ['#a50026ff', '#ffffbfff', '#313695ff'], # 3 classes
-            ['#a50026ff', '#fdbf71ff', '#bde2eeff', '#313695ff'], # 4 classes
-            ['#a50026ff', '#f88d52ff', '#ffffbfff', '#90c3ddff', '#313695ff'], # 5 classes
-            ['#a50026ff', '#f46d43ff', '#fee090ff', '#e0f3f8ff', '#74add1ff', '#313695ff'], # 6 classes
-            ['#a50026ff', '#ea593aff', '#fdbf71ff', '#ffffbfff', '#bde2eeff', '#649ac7ff', '#313695ff'], # 7 classes
-            ['#a50026ff', '#e34a33ff', '#fca55dff', '#fee99dff', '#e9f6e8ff', '#a3d3e6ff', '#598dc0ff', '#313695ff'], # 8 classes
-            ['#a50026ff', '#de3f2eff', '#f88d52ff', '#fed384ff', '#ffffbfff', '#d3ecf4ff', '#90c3ddff', '#5183bbff', '#313695ff'], # 9 classes
-            ['#a50026ff', '#da372aff', '#f67b4aff', '#fdbf71ff', '#feeea5ff', '#eef8dfff', '#bde2eeff', '#80b7d6ff', '#4a7bb7ff', '#313695ff'], # 10 classes
-
-        ]
     """
- 
-    
-    hex_colors = []
-    for i in range(2, n_classes):
-        cmap = matplotlib.cm.get_cmap(cmap_name, i)
-        hex_colors.append(
-            [
-                matplotlib.colors.to_hex(rgb, keep_alpha=True)
-                for rgb in cmap(np.arange(0, cmap.N))
-            ]
-        )
 
-    hex_colors.insert(0, None)
-    hex_colors.insert(0, None)
-    
+    hex_colors = color_blind_friendly_colors.copy()
+    if n_classes:
+        for i in range(len(color_blind_friendly_colors), n_classes + 1):
+            cmap = matplotlib.cm.get_cmap(cmap_name, i)
+            hex_colors.append(
+                [
+                    matplotlib.colors.to_hex(rgb, keep_alpha=True)
+                    for rgb in cmap(np.arange(0, cmap.N))
+                ]
+            )
+
     return hex_colors
 
 
 COLORS = {'scatter_edge': GREY,
           'scatter_marker': BLUE,
           'scatter_marker_alpha': 0.7,
-          'class_boundary' : GREY,
-          'warning' : '#E9130D',
-          'tile_alpha':0.8,            # square tiling in clfviz to show probabilities
-          'tesselation_alpha': 0.3,    # rectangular regions for decision tree feature space partitioning
+          'class_boundary': GREY,
+          'warning': '#E9130D',
+          'tile_alpha': 0.8,  # square tiling in clfviz to show probabilities
+          'tesselation_alpha': 0.3,  # rectangular regions for decision tree feature space partitioning
           'tesselation_alpha_3D': 0.5,
           'split_line': GREY,
           'mean_line': '#f46d43',
@@ -117,7 +98,12 @@ COLORS = {'scatter_edge': GREY,
           }
 
 
-def adjust_colors(colors):
+def adjust_colors(colors, n_classes=None, cmp="RdYlBu"):
     if colors is None:
+        if n_classes and n_classes > len(color_blind_friendly_colors) - 1:
+            # in case the number of classes is bigger than the color_blind_friendly_colors can handle, we will add more
+            # color class values
+            COLORS["classes"] = get_hex_colors(n_classes, cmp)
         return COLORS
+
     return dict(COLORS, **colors)
