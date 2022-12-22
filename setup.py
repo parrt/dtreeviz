01d8f4d4ef3cf84692918f520fd7f750cd44e5e5
Terence Parr <parrt@antlr.org>
2022-12-22 15:22:11 -0800

    set Version to 2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtreeviz',
-    version='1.4.1',
+    version='2.0.0',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
