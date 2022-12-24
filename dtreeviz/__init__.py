e771e02bd284c8fcb32aa30ef971768aa5c4d9f2
Terence Parr <parrt@antlr.org>
2022-12-24 11:06:02 -0800

    restrict symbols imported via `import dtreeviz`
    Signed-off-by: Terence Parr <parrt@antlr.org>

--- dtreeviz/__init__.py
+++ dtreeviz/__init__.py
@@ -1,4 +1,22 @@
 from .version import __version__
 
 from dtreeviz.classifiers import decision_boundaries
-from dtreeviz.trees import *
+
+# OLD API
+from dtreeviz.trees import rtreeviz_univar, \
+    rtreeviz_bivar_heatmap, \
+    rtreeviz_bivar_3D, \
+    ctreeviz_univar, \
+    ctreeviz_bivar, \
+    dtreeviz, \
+    viz_leaf_samples, \
+    viz_leaf_criterion, \
+    ctreeviz_leaf_samples , \
+    viz_leaf_target, \
+    describe_node_sample, \
+    explain_prediction_path
+
+# NEW API
+# call m = dtreeviz.model(...) then m.view() etc...
+from dtreeviz.trees import DTreeViz, model
+
