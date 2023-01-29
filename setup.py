5fb3b3654fdfad0396bed6d8ab1b4cac2b8c0c31
Terence Parr <parrt@antlr.org>
2023-01-29 11:21:10 -0800

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_tensorflow = ['tensorflow_decision_forests']
 
 setup(
     name='dtreeviz',
-    version='2.1.2',
+    version='2.1.3',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
