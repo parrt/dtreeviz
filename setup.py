0550fa9da825a33c5592d96633486b0919fbcf95
parrt <parrt@antlr.org>
2023-04-28 10:13:28 -0700

    set v to 2.2.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_tensorflow = ['tensorflow_decision_forests']
 
 setup(
     name='dtreeviz',
-    version='2.2.1',
+    version='2.2.2',
     url='https://github.com/parrt/dtreeviz',
     license='MIT',
     packages=find_packages(),
